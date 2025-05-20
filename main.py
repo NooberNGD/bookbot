a1b736d5939285b795f2c2b453e4746bb0454bfd
josh <jpivato2@gmail.com>
2025-05-20 17:06:17 -0400

    Updated with command line arguments

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from stats import count_words, count_characters, sorted_list
+import sys
+
+args = sys.argv
+
+
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -19,12 +24,15 @@ def report(book_path, count, sorted_list):
 
 
 def main():
-    book_path = "books/frankenstein.txt"
+    try:
+        book_path = args[1]
+    except:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
     text = get_book_text(book_path)
     count = count_words(text)
     letter_count = count_characters(text)
     list = sorted_list(letter_count)
-    print(list)
     report(book_path, count, list)
 
 if __name__ == "__main__":
